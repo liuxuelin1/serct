c981bf33ba2989c3c7f1c14688db22c0c8ab02cc
liuxuelin1 <2788391410@qq.com>
2020-06-08 20:54:11 +0800

    提交人:刘学林 提交原因:修改代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*
 * 这个是git测试类
+* 这是我第一次使用git提交修改的代码
 * */
 @SpringBootApplication
 public class DemoApplication {
